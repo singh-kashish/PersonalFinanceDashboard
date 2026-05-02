ac68753589c5a15cbf9c1f190e322c7e6e645a62
Kashish Singh <your_verified_email@example.com>
2026-05-02 19:13:58 +0530

    Scaffold

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/",(req,res)=>{
+    res.send("hello")
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server Listening on Port-${PORT}`)
+})
