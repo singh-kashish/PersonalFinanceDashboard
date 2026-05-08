0e1d253e3b7b1a93aedc58a033bd7adf082097f7
Kashish Singh <your_verified_email@example.com>
2026-05-08 18:38:24 +0530

    Removed env, added controller for health test

--- server/src/controllers/health.controller.js
+++ server/src/controllers/health.controller.js
@@ -1,5 +1,7 @@
-const healthController = (req,res) => {
-    
+const request = require('supertest');
+
+const healthController = async (req,res) => {
+        res.status(200).json({status:"ok"})
 }
 
 module.exports = healthController
